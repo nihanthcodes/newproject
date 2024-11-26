4c03da0e79c9a3ab900f7654e559524e159db062
Nihanth Tatikonda <2015567@apps.nsd.org>
2024-11-26 15:14:38 -0800

    asdf

--- Board.java
+++ Board.java
@@ -1,24 +1,36 @@
+import java.awt.FlowLayout;
+import java.util.Arrays;
+
 import javax.swing.JFrame;
 
 public class Board extends JFrame{
     // properties
-    private char[][] STATE = new char[][]{};
+    private Location[][] STATE = new Location[][]{
+        {null, null, null},
+        {null, null, null},
+        {null, null, null}
+    };
     
     // constructor
     public Board(){
+        super();
+        super.setTitle("BOARD!");
+        super.setSize(400, 600);
+        super.setTitle("yashna");
+        super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        for (int i=0; i<9; i++){
+            this.STATE[i/3][i%3] = new Location(this, i%3, i/3);
+            System.out.println(Arrays.deepToString(STATE));
+        }
+        super.setLayout(new FlowLayout());
 
-        setTitle("yashna");
-        setSize(400, 300);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Location btn = new Location();
-        this.add(btn);
-
-        setVisible(true);
-        setLocationRelativeTo(null);
-
-
-
+        super.setVisible(true);
+        super.setLocationRelativeTo(null);
         System.out.println("A new board!");
+    }
+
+    public void printState(){
+        System.out.println(Arrays.deepToString(STATE));
     }
 }
--- Location.java
+++ Location.java
@@ -1,14 +1,41 @@
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
 
-public class Location extends JButton{
+public class Location extends JButton implements ActionListener{
     // properties
     private int x;
     private int y;
+    private Board b;
     private char activeSymbol;
+    private String symbol;
 
-    // constructor
     public Location(){
-        super("excellent");
+        super();
+        super.setText("asdfsadf");
     }
+    // constructor
+    public Location(Board b, int x, int y){
+        this.b = b;
+        this.x = x;
+        this.y = y;
+        b.add(this);
+        super.setPreferredSize(new Dimension(60, 60));
+        this.addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        this.symbol = "x";
+        this.setText("YEAH");
+        b.printState();        
+    }
+
+    @Override
+    public String toString(){
+        return symbol;  
+    }  
+    
 
 }
